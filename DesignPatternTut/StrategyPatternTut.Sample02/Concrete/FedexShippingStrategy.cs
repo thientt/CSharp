18ccef6e5fecff007d6558476ffdfdd5645bda6a
3SI-LAP016\thien.tt <thien.tt@3si.com.vn>
2017-04-28 16:47:16 +0700

    apply design pattern strategy

---
+++ DesignPatternTut/StrategyPatternTut.Sample02/Concrete/FedexShippingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using StrategyPatternTut.Sample02.Strategy;
+
+namespace StrategyPatternTut.Sample02.Concrete
+{
+    public class FedexShippingStrategy : IShippingStrategy
+    {
+        public double Calculate(Order order)
+        {
+            return 5.00d;
+        }
+    }
+}
---
+++ DesignPatternTut/StrategyPatternTut.Sample02/Services/ShippingCostCalculationService.cs
@@ -0,0 +1,32 @@
+﻿using StrategyPatternTut.Sample02.Strategy;
+using System;
+
+namespace StrategyPatternTut.Sample02.Services
+{
+    public class ShippingCostCalculationService
+    {
+        private IShippingStrategy _shippingStrategy;
+
+        public ShippingCostCalculationService()
+        {
+        }
+
+        public ShippingCostCalculationService(IShippingStrategy shippingStrategy)
+        {
+            _shippingStrategy = shippingStrategy;
+        }
+
+        public double CalculateShippingCost(Order order)
+        {
+            if (_shippingStrategy == null)
+                throw new ArgumentNullException("Shipping don't null");
+
+            return _shippingStrategy.Calculate(order);
+        }
+
+        public double CalculateShippingCost(Order order, Func<Order, double> shippingCost)
+        {
+            return shippingCost(order);
+        }
+    }
+}
